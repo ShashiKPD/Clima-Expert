67f2a0eaa6e4ad6bd516a427d981c90e290a601c
panditsamik <panditsamik25@gmail.com>
2024-07-12 19:07:58 +0530

    Added Top Buttons, Inputs, Time and Location

---
+++ src/components/TempAndDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TempAndDetails = () => {
+  return <div>TempAndDetails</div>;
+};
+
+export default TempAndDetails;
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-xl font-extralight">
+          Friday, 12 July 2024 | Local time: 06:43 PM
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-3xl font-medium">Berlin, DE</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLocation;
