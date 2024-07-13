51db9bc021fd9d8be4b7ed4bfec1bd80b4217a4d
panditsamik <panditsamik25@gmail.com>
2024-07-13 22:16:20 +0530

    Handle Enter key press

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -6,7 +6,15 @@ const Inputs = ({ setQuery, setUnits }) => {
 
   const handleSearchClick = () => {
     if (city !== "") setQuery({ q: city });
-    console.log("search button clicked")
+    console.log("search button clicked");
+  };
+
+  // Function called when enter is clicked
+  const handleFormClick = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault(); // Prevents default form submission behavior, i.e, reload
+      handleSearchClick();
+    }
   };
 
   const handleLocationClick = () => {
@@ -14,8 +22,7 @@ const Inputs = ({ setQuery, setUnits }) => {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
         setQuery({ lat: latitude, lon: longitude });
-        console.log("location button clicked")
-
+        console.log("location button clicked");
       });
     }
   };
@@ -23,13 +30,14 @@ const Inputs = ({ setQuery, setUnits }) => {
   return (
     <div className="flex flex-row justify-center my-6">
       <div
-        className="flex flex-row w-3/4 items-center justify-center 
+        className="flex flex-row w-2/4 items-center justify-center 
       space-x-4"
       >
         <input
           type="text"
           onChange={(e) => setCity(e.currentTarget.value)}
           placeholder="Search by city..."
+          onKeyDown={handleFormClick} // Handle Enter key press
           className="text-gray-500 text-xl font-light p-2 
           w-full shadow-xl capitalize focus:outline-none 
           placeholder:lowercase"
